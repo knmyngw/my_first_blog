72368931c4df0ddf31a0c9c67d48e3ed68a98659
knmyngw <knm.yngw@gmail.com>
2023-10-24 18:04:24 +0900

    tutorial_extention

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+""" add """
+LOGIN_REDIRECT_URL = '/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 
 # mysite/urls.py
 
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('knm66/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 ]
